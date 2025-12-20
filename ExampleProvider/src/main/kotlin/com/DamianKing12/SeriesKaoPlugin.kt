46d66dc8d4d952ef5e03f0a7990540bdfd9f0b1e
DamianKing12 <damian.a.king.r.12@gmail.com>
2025-12-20 11:59:12 -0400

    Update SeriesKaoPlugin.kt

--- ExampleProvider/src/main/kotlin/com/DamianKing12/SeriesKaoPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/DamianKing12/SeriesKaoPlugin.kt
@@ -1,1 +1,12 @@
+package com.DamianKing12
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class SeriesKaoPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(SeriesKaoProvider())
+    }
+}

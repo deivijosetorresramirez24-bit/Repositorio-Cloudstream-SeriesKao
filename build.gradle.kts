4c6140b9954633a6d1f243bde0de3f2e92d9a21e
deivijosetorresramirez24-bit <deivijosetorresramirez24@gmail.com>
2025-12-23 15:05:19 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,13 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // Usamos un hash fijo para que Gradle no se pierda buscando 'master'
         classpath("com.github.recloudstream:gradle:cce1b8d84d")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
+// Eliminamos el bloque allprojects para limpiar los errores de configuración
+// Los repositorios ya se manejan correctamente en settings.gradle.kts
 
 task("clean", Delete::class) {
     delete(layout.buildDirectory)
